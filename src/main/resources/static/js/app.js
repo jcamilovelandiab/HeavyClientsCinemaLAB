6d5f82cd379d1a3db004d530a8ab27501ae1fbcf
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-03-18 02:36:55 -0500

    ya funciona el api mock por fin
    Co-authored-by: John <JhonhRge@gmail.com>

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,17 @@
+app=(function(){
+	
+	return{
+		getCinemaByName:function(cinemaname){
+			
+			return apimock.getCinemaByName(cinemaname, function(cinema){
+				$("#tableCinema > tr").remove(); //limpia la tabla para volver a llenar los datos
+				var table = $("#tableCinema");
+				for(let c of cinema[0].functions){
+					table.append("<tr><td>"+c.movie.name+"</td><td>"+c.seats.length+"</td><td>"+c.date+"</td><tr>");
+				}
+			})
+			
+		}
+	}
+	
+})();
