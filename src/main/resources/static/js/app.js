68ea175ce975186ffe365bbe00247d3aa56ffbac
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-03-18 14:17:18 -0500

    imagenes
    Co-authored-by: John <JhonhRge@gmail.com>

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,9 +6,12 @@ app=(function(){
 			return apimock.getCinemaByName(cinemaname, function(cinema){
 				$("#tableCinema > tr").remove(); //limpia la tabla para volver a llenar los datos
 				var table = $("#tableCinema");
+				
 				for(let c of cinema[0].functions){
 					table.append("<tr><td>"+c.movie.name+"</td><td>"+c.seats.length+"</td><td>"+c.date+"</td><tr>");
 				}
+				
+				$("#cinemaName").text(cinema.name);
 			})
 			
 		}
